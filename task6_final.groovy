6caad44b94b9f42dd4d73cc8d354189955253040
Charu Chandak <imcharuchandak27@gmail.com>
2020-07-25 17:43:12 +0530

    1st

--- task6_final.groovy
+++ task6_final.groovy
@@ -8,7 +8,7 @@ description ("Job to pull code from GitHub repository")
     }
    steps {
         shell('''
-cp -rvf * /root/task6
+sudo cp -rvf * /root/task6
 ''')
      
         remoteShell('root@192.168.1.103:22') {
